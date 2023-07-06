87b39d15449764aa35cda2c246e9fec9c497279b
AntonioSpn <antonio941927@hotmail.it>
2023-07-06 18:38:44 +0200

    Aggiunta classe Film

---
+++ src/Film.java
@@ -0,0 +1,42 @@
+import java.util.Date;
+
+public class Film {
+    private String title;
+    private String director; // regista
+    private Date date; // data di uscita del film al cinema
+
+
+    public Film(String title, String director, Date date) {
+        this.title = title;
+        this.director = director;
+        this.date = date;
+    }
+
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return this.director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+
+}
+
