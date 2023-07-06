252ffbc75a300ee87f8a3dc73f00ed0270eefa7b
AntonioSpn <antonio941927@hotmail.it>
2023-07-06 18:38:30 +0200

    Aggiunta classe cinema

---
+++ src/Cinema.java
@@ -0,0 +1,42 @@
+public class Cinema {
+    
+    private String city;
+    private int room; //numero sale del cinema
+    private Owner owner;
+
+
+
+    public Cinema(String city, int room, Owner owner) {
+        this.city = city;
+        this.room = room;
+        this.owner = owner;
+    }
+
+
+
+    public String getCity() {
+        return this.city;
+    }
+
+    
+    public int getRoom() {
+        return this.room;
+    }
+
+    public void setRoom(int room) {
+        this.room = room;
+    }
+
+    public Owner getOwner() {
+        return this.owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+
+
+
+    
+}
