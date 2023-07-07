6cb40c7e54004b2e42ca285c7c8ead2ab207b749
AntonioSpn <antonio941927@hotmail.it>
2023-07-07 19:06:14 +0200

    Aggiunta toString e Arraylist Ticket

--- src/Customer.java
+++ src/Customer.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
+
 public class Customer extends User{
-    //TODO Arraylist<Tickets> Biglietti;
+    private ArrayList<Ticket> Tickets;
 
     public Customer(String name, String surname, String username, String email, String password) {
         super(name, surname, username, email, password);
+
+        Tickets = new ArrayList<Ticket>();
+    }
+
+    public void buyTicket (Ticket ticket){
+        Tickets.add(ticket);
+    }
+
+    public void viewTickets (){
+        System.out.println(Tickets.toString());
     }
 
 }
