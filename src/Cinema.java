95af448ad5714361e016c3a6eb4ccde29d8f90ee
AntonioSpn <antonio941927@hotmail.it>
2023-07-18 12:15:29 +0200

    Pulizia classi

--- src/Cinema.java
+++ src/Cinema.java
@@ -8,15 +8,12 @@ public class Cinema implements Serializable{
     private int[] room; //numero sale del cinema
     private ArrayList <Film> listFilm;
 
-
-
     public Cinema(String name, String city, int[] room, ArrayList<Film> listFilm) {
         this.name = name;
         this.city = city;
         this.room = room;
         this.listFilm = listFilm;
     }
-
     
     public Cinema(String name, String city, int[] room) {
         this.name = name;
@@ -25,24 +22,17 @@ public class Cinema implements Serializable{
         this.listFilm = new ArrayList<>();
     }
 
-
-
-
     public String getName() {
         return this.name;
-    }
-
-    
+    }    
 
     public String getCity() {
         return this.city;
     }
 
-
     public int[] getRoom() {
         return this.room;
     }
-
     
     public ArrayList<Film> getListFilm() {
         return this.listFilm;
@@ -58,23 +48,11 @@ public class Cinema implements Serializable{
 
     public ArrayList <Film> viewToday (Date date){
         ArrayList<Film> tmp = new ArrayList<>();
-
         for (Film film : this.listFilm) {
             if(film.getStartDate().compareTo(date) <1 && film.getEndDate().compareTo(date) > 0 ){ //
                 tmp.add(film);
             }
         }
-
         return tmp;
     }
-
-    
-
-
-
-
-
-
-
-
 }
